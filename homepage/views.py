f6545e641edb0e81211803904b5e0bea12b686e4
Anirudh <anirudh18@iitg.ac.in>
2020-03-20 00:56:06 +0530

    Added Heat Map

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
+from datetime import datetime, date
 import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
@@ -33,20 +34,45 @@ def submission_statistics(request, handle):
     runtime_error = 0
     hacked = 0
     compilation_error = 0
+    submissions = {}
+    today = date.today()
     for rows in data['result']:
-        verdict = rows['verdict']
-        if verdict == "OK":
-            accepted += 1
-        elif verdict == "HACKED":
-            hacked += 1
-        elif verdict == "WRONG_ANSWER":
-            wrong_answer += 1
-        elif verdict == "TIME_LIMIT_EXCEEDED":
-            time_exceed += 1
-        elif verdict == "RUNTIME_ERROR":
-            runtime_error += 1
-        else:
-            compilation_error += 1
+        time = rows['creationTimeSeconds']
+        s_day = datetime.fromtimestamp(time).date()
+        days = (today - s_day).days
+        week = days // 7
+        day = days % 7 + 1
+        if week <= 3:
+            if submissions.get(week) is None:
+                submissions[week] = {day: 1}
+            else:
+                dic = submissions[week]
+                if dic.get(day) is None:
+                    dic[day] = 1
+                else:
+                    dic[day] += 1
+        if rows.get('verdict') is not None:
+            verdict = rows['verdict']
+            if verdict == "OK":
+                accepted += 1
+            elif verdict == "HACKED":
+                hacked += 1
+            elif verdict == "WRONG_ANSWER":
+                wrong_answer += 1
+            elif verdict == "TIME_LIMIT_EXCEEDED":
+                time_exceed += 1
+            elif verdict == "RUNTIME_ERROR":
+                runtime_error += 1
+            else:
+                compilation_error += 1
+
+
+    for key in submissions:
+        dic = submissions[key]
+        for i in range(1, 8):
+            if dic.get(i) is None:
+                dic[i] = 0
+
     context = {
         'handle': handle,
         'accepted': accepted,
@@ -54,9 +80,15 @@ def submission_statistics(request, handle):
         'runtime_error': runtime_error,
         'wrong_answer': wrong_answer,
         'time_exceed': time_exceed,
-        'compilation_error': compilation_error
+        'compilation_error': compilation_error,
     }
-    return render(request, "homepage/submission_statistics.html", context)
+    for key in submissions:
+        dic = submissions[key]
+        for i in range(1, 8):
+            context['day'+str(key)+str(i)] = dic[i]
+    print(context)
+    return render(request, "homepage/statistics.html", context)
+
 
 def contest_statistics(request, handle):
     page_url = "https://codeforces.com/api/user.rating?handle=" + handle
